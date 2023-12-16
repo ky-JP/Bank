e89e5d2b598f7367f414caddb7b661731abe409b
João Paulo <joaopauloiff2018@gmail.com>
2023-12-16 10:28:18 -0300

    implementing setLocale

--- src/Program.java
+++ src/Program.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter account number: ");
@@ -48,6 +49,8 @@ public class Program {
 
         System.out.println("Updated account data: ");
         System.out.println(account);
+
+        input.close();
 
     }
 }
